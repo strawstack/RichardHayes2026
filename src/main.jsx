89663861c97e9e66ef2523bf3b9bece8920de8aa
Richard Hayes <rhshadowgreen@gmail.com>
2026-01-11 18:29:29 -0800

    Add tailwind and react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="about" element={<div>about</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
