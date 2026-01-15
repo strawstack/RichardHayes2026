3d67897efa20d5ed1a047a586ab43b5265317ecb
Richard Hayes <rhshadowgreen@gmail.com>
2026-01-14 20:04:02 -0800

    Gray box implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import i128 from './images/128.png';
 import i568x128 from './images/568x128.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -7,8 +6,6 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { cloneElement, useState, type JSX } from 'react';
-import cn from 'classnames';
-
 library.add(fas, far, fab);
 
 type Props = {
@@ -22,36 +19,32 @@ const text = {
 
 const SqImg = () => {
     return (
-        <div className='w-[128px] bg-gray-700'></div>
+        <div className='w-[128px] bg-gray-400'></div>
     );
 };
 
 const FullImg = () => {
     return (
-        <div className='h-[128px] overflow-hidden'>
-            <img src={i568x128}></img>
+        <div className='h-[128px] bg-gray-400'>
+            {/* <img src={i568x128}></img> */}
         </div>
     );
 };
 
 function App() {
 
-    const [open, setOpen] = useState(false);
-
-    function Details({children}: Props) {
-        return (<> {open && children} </>);
-    }
-
-    Details.Summary = ({children: trigger}: {children: JSX.Element}) => {
-        const clone = cloneElement(trigger, {
-            onClick: () => { setOpen(!open) }
+    function Details({open, close, summary, details}: Props) {
+        const [expand, setExpand] = useState(false);
+        const clone = cloneElement(summary(expand ? open : close), {
+            onClick: () => { setExpand(!expand) }
         });
-        return clone;
-    };
-
-    Details.Content = ({children}: Props) => {
-        return clone;
-    };
+        return (
+            <div className='flex flex-col p-4 gap-y-4 border-2'>
+                {clone}
+                {expand && details}
+            </div>
+        );
+    }
 
     return (
         <div className='min-h-screen flex justify-center'>
@@ -59,12 +52,12 @@ function App() {
 
                 <FullImg />
 
-                <div className=''>{text.intro}</div>
-
-                <FullImg />
+                <div className='p-4 border-2'>
+                    <div className=''>{text.intro}</div>
+                </div>
 
                 <div className='grid grid-cols-[1fr_auto] gap-x-2'>
-                    <div className='flex flex-col gap-y-2 p-4 border-2'>
+                    <div className='flex flex-col gap-y-2 p-4 border-t-2 border-b-2 border-l-2'>
                         <div className='flex justify-between'>
                             <div className='flex gap-x-2 items-center'>
                                 <div className='text-4xl'>Games</div>
@@ -92,7 +85,7 @@ function App() {
 
                 <div className='grid grid-cols-[auto_1fr] gap-x-2'>
                     <SqImg />
-                    <div className='flex flex-col gap-y-2 p-4 border-2'>
+                    <div className='flex flex-col gap-y-2 p-4 border-t-2 border-r-2 border-b-2'>
                         <div className='flex justify-between'>
                             <div className='flex gap-x-2 items-center'>
                                 <div className='text-4xl'>Videos</div>
@@ -107,7 +100,7 @@ function App() {
                 </div>
 
                 <div>
-                    <div className='flex flex-col gap-y-2 p-4 border-2'>
+                    <div className='flex flex-col gap-y-2 p-4 border-l-2 border-r-2'>
                         <div className='flex justify-between'>
                             <div className='flex gap-x-2 items-center'>
                                 <div className='text-4xl'>Music</div>
@@ -124,7 +117,7 @@ function App() {
                 <FullImg />
 
                 <div>
-                    <div className='flex flex-col gap-y-2 p-4 border-2'>
+                    <div className='flex flex-col gap-y-2 p-4 border-t-2 border-b-2'>
                         <div className='flex justify-between'>
                             <div className='flex gap-x-2 items-center'>
                                 <FontAwesomeIcon icon={["fas", "podcast"]} size="lg" />
@@ -151,27 +144,48 @@ function App() {
                     </div>
                 </div>
 
-                <Details className='flex flex-col p-4 gap-y-4 border-2'>
-                    <Details.Summary>
-                        <div className='grid grid-cols-[1fr_auto] gap-x-2'>
-                            <div className='flex flex-col gap-y-2'>
-                                <div className='flex gap-x-2 items-center'>
-                                    <div className='text-4xl'>Title</div>
+                <Details
+                    open={<FontAwesomeIcon icon={["fas", "chevron-down"]} size="lg" />}
+                    close={<FontAwesomeIcon icon={["fas", "chevron-right"]} size="lg" />}
+                    summary={
+                        (icon: JSX.Element) => { return (
+                            <div className='grid grid-cols-[1fr_auto] gap-x-2'>
+                                <div className='flex flex-col gap-y-2'>
+                                    <div className='flex gap-x-2 items-center'>
+                                        <div className='text-4xl'>Title</div>
+                                    </div>
+                                    <div className=''>This is the description of games. I talk about what my idea is with games.</div>
                                 </div>
-                                <div className=''>This is the description of games. I talk about what my idea is with games.</div>
+                                <div className='flex items-center'>{icon}</div>
                             </div>
-                            <div className='flex items-center'>
-                                <FontAwesomeIcon icon={["fas", "chevron-right"]} size="lg" />
-                            </div>
-                        </div>
-                    </Details.Summary>
-                    <Details.Content>
-                        <div className='w-[500px] h-[300px] bg-gray-700
+                        )}
+                    }
+                    details={
+                        <div className='w-[500px] h-[300px] bg-gray-400
                             flex justify-center items-center'
                         >Game Area</div>
-                    </Details.Content>
-                </Details>
+                    }
+                />
 
+                <div className='flex justify-center p-4 gap-x-8'>
+                    <div className='border-2 rounded-full p-4'>
+                        <FontAwesomeIcon icon={["fas", "gamepad"]} size="lg" />
+                    </div>
+                    <div className='border-2 rounded-full p-4'>
+                        <FontAwesomeIcon icon={["fas", "gamepad"]} size="lg" />
+                    </div>
+                    <div className='border-2 rounded-full p-4'>
+                        <FontAwesomeIcon icon={["fas", "gamepad"]} size="lg" />
+                    </div>
+                    <div className='border-2 rounded-full p-4'>
+                        <FontAwesomeIcon icon={["fas", "gamepad"]} size="lg" />
+                    </div>
+                </div>
+
+                <FullImg />
+                
+                <FullImg />
+                
                 <FullImg />
 
             </div>
