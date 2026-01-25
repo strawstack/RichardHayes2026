6592c9c1788051e3f989702e0d22d6cd825fd271
Richard Hayes <rhshadowgreen@gmail.com>
2026-01-24 16:01:16 -0800

    working circle generator, but points in circle should move so sharp edges don't appear

--- src/components/backgroundCanvas.tsx
+++ src/components/backgroundCanvas.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useRef, type RefObject } from "react";
+import { useEffect, useRef, useState, type RefObject } from "react";
 import { useAnimationFrame } from "../hooks/useAnimationFrame";
 import { useCanvasSize } from "../hooks/useCanvasSize";
 import { useCanvasContext } from "../hooks/useCanvasContext";
 
 export function BackgroundCanvas({
   draw,
+  onInit = () => {},
   onResize = () => {},
   isAnimated = false,
 }: {
@@ -15,12 +16,14 @@ export function BackgroundCanvas({
     width: number;
     delta: number | undefined;
   }) => void;
+  onInit?: (height: number, width: number) => void;
   onResize?: (canvas: HTMLCanvasElement, height: number, width: number) => void;
   isAnimated?: boolean;
 }) {
   const canvas = useRef<HTMLCanvasElement>(null);
   const size = useCanvasSize(canvas);
   const ctx = useCanvasContext(canvas);
+  const [initOnce, setInitOnce] = useState(false);
 
   function handleResize() {
     if (canvas.current && size) {
@@ -31,33 +34,40 @@ export function BackgroundCanvas({
 
   useAnimationFrame(
     (delta) => {
-      if (canvas.current && size && ctx && isAnimated) {
+      if (canvas.current && size && ctx && isAnimated && initOnce) {
         const { height, width } = size;
         canvas.current.width = width;
         canvas.current.height = height;
         draw({ canvas: canvas.current, ctx, height, width, delta });
       }
     },
-    [isAnimated, canvas.current, size, ctx],
+    [canvas.current, size, ctx, isAnimated, initOnce],
   );
 
   useEffect(() => {
-    if (canvas.current && size && ctx) {
+    if (canvas.current && size && ctx && initOnce) {
       window.addEventListener("resize", handleResize);
     }
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, [canvas, size, ctx]);
+  }, [canvas, size, ctx, initOnce]);
 
   useEffect(() => {
-    if (canvas.current && size && ctx) {
+    if (canvas.current && size && ctx && initOnce) {
       const { height, width } = size;
       canvas.current.width = width;
       canvas.current.height = height;
       draw({ canvas: canvas.current, ctx, height, width, delta: undefined });
     }
-  }, [canvas, size, ctx]);
+  }, [canvas, size, ctx, initOnce]);
+
+  useEffect(() => {
+    if (canvas.current && size && ctx && !initOnce) {
+      onInit(size.height, size.width);
+      setInitOnce(true);
+    }
+  }, [canvas, size, ctx, initOnce]);
 
   return (
     <canvas
--- src/components/backgroundDrops.tsx
+++ src/components/backgroundDrops.tsx
@@ -1,6 +1,5 @@
 import { useMemo, useState } from "react";
-import { BackgroundCanvas } from "./BackgroundCanvas";
-import { height, width } from "@fortawesome/free-brands-svg-icons/fa11ty";
+import { BackgroundCanvas } from "./backgroundCanvas";
 
 function angleToPoint(radius: number, angle: number): [number, number] {
   const clockwise = -1;
@@ -16,66 +15,97 @@ type Circle = {
   cy: number;
   radius: number;
   numPoints: number;
-  initVary: number;
+  varySize: number;
+  varySpeed: number;
   angle: number;
   initAngle: number;
   duration: number; // remaining time to fade out
   angleValues: number[];
-  varyValues: number[];
+  varySpeeds: number[];
+  render: (ctx: CanvasRenderingContext2D, delta: number | undefined) => void;
 };
 
+type Size = { height: number; width: number };
+
+function circle({ height, width }: Size): Circle {
+  const cx = Math.random() * width;
+  const cy = Math.random() * height;
+  const radius = Math.random() * 100 + 50;
+  const numPoints = 64;
+  const varySize = 6;
+  const varySpeed = 0.01;
+  const angle = (2 * Math.PI) / numPoints;
+  const initAngle = Math.random() * angle;
+  const angleValues = Array(numPoints)
+    .fill(null)
+    .map((_, i) => i * angle + initAngle);
+  const vary = (amount: number) => Math.random() * 2 * amount - amount;
+  let varyValues = Array(numPoints)
+    .fill(null)
+    .map(() => vary(varySize));
+  let varySpeeds = Array(numPoints)
+    .fill(null)
+    .map(() => varySpeed);
+  return {
+    cx,
+    cy,
+    radius,
+    numPoints,
+    varySize,
+    varySpeed,
+    angle,
+    initAngle,
+    duration: 1000,
+    angleValues,
+    varySpeeds,
+    render(ctx: CanvasRenderingContext2D, delta: number | undefined = 1) {
+      const flipSpeeds = (speeds: number[]) =>
+        speeds.map((s) => {
+          const flip = Math.random() < 0.1;
+          return flip ? -1 * s : s;
+        });
+      const clamp = (v: number) =>
+        Math.max(Math.min(v, varySize), -1 * varySize);
+      varySpeeds = flipSpeeds(varySpeeds);
+      varyValues = varyValues.map((v, i) => clamp(v + varySpeeds[i] * delta));
+      const points: [number, number][] = angleValues
+        .map((v, i) => angleToPoint(radius + varyValues[i], v))
+        .map(([x, y]) => [x + cx, y + cy]);
+      ctx.beginPath();
+      ctx.moveTo(...points[0]);
+      points.slice(1).forEach(([x, y]) => ctx.lineTo(x, y));
+      ctx.closePath();
+      ctx.lineWidth = 3;
+      ctx.stroke();
+    },
+  };
+}
+
 export function BackgroundDrops() {
-  const [size, setSize] = useState({ height: 0, width: 0 });
+  const [size, setSize] = useState<{ height: number; width: number } | null>(
+    null,
+  );
+
   const circles: Circle[] = useMemo(() => {
-    const numCircles = 10;
-    return Array(numCircles)
-      .fill(null)
-      .map(() => {
-        const cx = Math.random() * size.width;
-        const cy = Math.random() * size.height;
-        const radius = 50;
-        const numPoints = 24;
-        const initVary = radius * 0.025;
-        const angle = (2 * Math.PI) / numPoints;
-        const initAngle = Math.random() * angle;
-        const angleValues = Array(numPoints)
-          .fill(null)
-          .map((_, i) => i * angle + initAngle);
-        const vary = (amount: number) => Math.random() * 2 * amount - amount;
-        const varyValues = Array(numPoints)
-          .fill(null)
-          .map(() => vary(initVary));
-        return {
-          cx,
-          cy,
-          radius,
-          numPoints,
-          initVary,
-          angle,
-          initAngle,
-          duration: 1000,
-          angleValues,
-          varyValues,
-        };
-      });
+    if (size) {
+      const numCircles = 10;
+      return Array(numCircles)
+        .fill(null)
+        .map(() => circle(size));
+    } else {
+      return [];
+    }
   }, [size]);
 
   return (
     <BackgroundCanvas
-      isAnimated={false}
+      isAnimated={true}
+      onInit={(height, width) => {
+        setSize({ height, width });
+      }}
       draw={({ ctx, height, width, delta }) => {
         ctx.clearRect(0, 0, height, width);
-        setSize({ height, width });
-        for (let { cx, cy, radius, angleValues, varyValues } of circles) {
-          const points: [number, number][] = angleValues
-            .map((v, i) => angleToPoint(radius + varyValues[i], v))
-            .map(([x, y]) => [x + cx, y + cy]);
-          ctx.beginPath();
-          ctx.moveTo(...points[0]);
-          points.slice(1).forEach(([x, y]) => ctx.lineTo(x, y));
-          ctx.closePath();
-          ctx.fill();
-        }
+        circles.forEach((circle) => circle.render(ctx, delta));
       }}
     ></BackgroundCanvas>
   );
