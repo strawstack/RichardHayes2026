094bddfaf5414ce5cc6e6fd25f5a4ffbacb13f80
Richard Hayes <rhshadowgreen@gmail.com>
2026-01-30 22:07:39 -0800

    Create helper functions to handle creation of circles and control points

--- src/utils.ts
+++ src/utils.ts
@@ -56,3 +56,84 @@ function dist(a: Point, b: Point) {
 function angleBetween(a: Point, b: Point) {
   return Math.acos(dot(a, b) / (mag(a) * mag(b)));
 }
+
+function controlVector(a: Point, b: Point) {
+  const angle = Math.acos(dot(a, b) / (mag(a) * mag(b)));
+  const half = angle / 2;
+  const cross = a.x * b.y - a.y * b.x > 0;
+  const controlAngle = Math.atan2(b.y, b.x) + (cross ? -1 * half : half);
+  return {
+    x: Math.cos(controlAngle),
+    y: Math.sin(controlAngle),
+  };
+}
+
+function getControlPoints(a: Point, controlVector: Point, bump: number) {
+  const c1 = sub(a, mul(controlVector, bump));
+  const c2 = add(a, mul(controlVector, bump));
+  return {
+    c1,
+    c2,
+  };
+}
+
+export function getCirclePoints(n: number) {
+  const angle = (2 * Math.PI) / n;
+  return Array(n)
+    .fill(null)
+    .map((_, i) => i * angle)
+    .map(() => {
+      return {
+        x: Math.cos(angle),
+        y: Math.sin(angle),
+      };
+    });
+}
+
+export function formatCircle(points: Point[], radius: number, center: Point) {
+  return points.map((p) => add(mul(p, radius), center));
+}
+
+export function controlPointsForCircle(circlePoints: Point[]) {
+  // Move last to first, so i-th control points align with i-th point
+  const copyCirclePoints = { ...circlePoints };
+  const last = copyCirclePoints.pop()!;
+  copyCirclePoints.unshift(last);
+  const length = copyCirclePoints.length;
+  const controlPoints = [];
+  for (let i = 0; i < length; i++) {
+    const a = copyCirclePoints[i];
+    const b = copyCirclePoints[(i + 1) % length];
+    const c = copyCirclePoints[(i + 2) % length];
+    const n = sub(b, a);
+    const m = sub(c, b);
+    const bump = 40;
+    const { c1, c2 } = getControlPoints(b, controlVector(n, m), bump);
+    controlPoints.push({ c1, c2 });
+  }
+  return controlPoints;
+}
+
+export function renderCircle(
+  ctx: CanvasRenderingContext2D,
+  p: Point[],
+  c: { c1: Point; c2: Point }[],
+) {
+  ctx.beginPath();
+  ctx.strokeStyle = "#333";
+  ctx.lineWidth = 2;
+  ctx.moveTo(p[0].x, p[0].y);
+  const len = p.length;
+  for (let i = 0; i < len; i++) {
+    const i1 = (i + 1) % len;
+    ctx.bezierCurveTo(
+      c[i].c2.x,
+      c[i].c2.y,
+      c[i1].c1.x,
+      c[i1].c1.y,
+      p[i1].x,
+      p[i1].y,
+    );
+  }
+  ctx.stroke();
+}
