b466302fcc83ee5afe96d65c8874979493b0f42f
RichardHayes <rhshadowgreen@gmail.com>
2026-01-31 21:39:14 -0800

    Full screen quad rendering red mesh at /shader route

--- src/routes/Shader.tsx
+++ src/routes/Shader.tsx
@@ -1,3 +1,31 @@
+import * as THREE from "three";
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrthographicCamera } from "@react-three/drei";
+
+function FullscreenQuad() {
+  const { viewport } = useThree();
+
+  return (
+    <mesh scale={[viewport.width, viewport.height, 1]}>
+      <planeGeometry args={[1, 1]} />
+      <meshBasicMaterial color="red" />
+    </mesh>
+  );
+}
+
 export function Shader() {
-  return <div className="relative min-h-screen">shader</div>;
+  return (
+    <Canvas>
+      <OrthographicCamera
+        makeDefault
+        top={1}
+        bottom={-1}
+        left={-1}
+        right={1}
+        near={-1}
+        far={1}
+      />
+      <FullscreenQuad />
+    </Canvas>
+  );
 }
