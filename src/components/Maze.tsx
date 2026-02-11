994b3650cbc283fcaea90793db13e79ab2831dab
Richard Hayes <rhshadowgreen@gmail.com>
2026-02-10 19:09:11 -0800

    Working node and edge generation, but not centered. Issue with calculation of padding space

--- src/components/Maze.tsx
+++ src/components/Maze.tsx
@@ -1,9 +1,218 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef, useState, type RefObject } from "react";
 
-export function Maze() {
-  const svgRef = useRef(null);
+type Point = { x: number; y: number };
+type Size = { width: number; height: number };
+type nid = string;
+type eid = string;
 
-  useEffect(() => {}, []);
+enum Kind {
+  Node = "Node",
+  Edge = "Edge",
+}
 
-  return <svg className="h-full w-full bg-teal-300" ref={svgRef}></svg>;
+type Node = {
+  id: nid;
+  pos: Point;
+  size: Size;
+  kind: Kind;
+  edges?: {
+    top: eid | undefined;
+    right: eid | undefined;
+    down: eid | undefined;
+    left: eid | undefined;
+  };
+};
+
+type Edge = {
+  id: eid;
+  pos: Point;
+  size: Size;
+  kind: Kind;
+  from?: nid;
+  to?: nid;
+};
+
+type Maze = {
+  nodes: { [key: nid]: Node };
+  edges: { [key: eid]: Edge };
+};
+
+const even = (r: number) => r % 2 === 0;
+
+function getSize(y: number, x: number, big: number, small: number): Size {
+  if (even(y) && even(x)) {
+    return { width: big, height: big };
+  } else if (even(y) && !even(x)) {
+    return { width: small, height: big };
+  } else if (!even(y) && even(x)) {
+    return { width: big, height: small };
+  } else {
+    // (!even(y) && !even(x))
+    return { width: small, height: small };
+  }
+}
+
+function getTopLeft(y: number, x: number, big: number, small: number): Size {
+  return {
+    width: even(x)
+      ? (x / 2) * (big + small)
+      : ((x - 1) / 2) * (big + small) + big,
+    height: even(y)
+      ? (y / 2) * (big + small)
+      : ((y - 1) / 2) * (big + small) + big,
+  };
+}
+
+function generateMaze({
+  width,
+  height,
+}: {
+  width: number;
+  height: number;
+}): Maze {
+  // The size of a Node or Room
+  const NODE_SIZE = 12;
+
+  // The width/height of the smaller dimension of a door
+  const EDGE_SIZE = 6;
+
+  const MIN_PAD = 4;
+  // Min pad + space for outer wall
+  const PAD = MIN_PAD + EDGE_SIZE;
+
+  // Size of actual space maze has to take up
+  const mazeWidth = width - 2 * PAD;
+  const mazeHeight = height - 2 * PAD;
+
+  // The smallest possible maze size (2 nodes with one edge between them)
+  const minSize = 2 * NODE_SIZE + EDGE_SIZE;
+
+  // Calculating number of nodes and edges that can fit
+  // starting from minSize
+  const pCols = Math.floor((mazeWidth - minSize) / (NODE_SIZE + EDGE_SIZE));
+  const pRows = Math.floor((mazeHeight - minSize) / (NODE_SIZE + EDGE_SIZE));
+
+  // The real padding value including space left over after rooms are added
+  const PAD_X = (width - pCols * (NODE_SIZE + EDGE_SIZE) + NODE_SIZE) / 2;
+  const PAD_Y = (height - pRows * (NODE_SIZE + EDGE_SIZE) + NODE_SIZE) / 2;
+
+  // Nodes and edges count as rows
+  const COLS = 2 * pCols + 1;
+  const ROWS = 2 * pRows + 1;
+
+  const graph: Node[] = Array(ROWS)
+    .fill(null)
+    .map((_, y) =>
+      Array(COLS)
+        .fill(null)
+        .map((_, x) => {
+          const { width: CELL_W, height: CELL_H } = getSize(
+            y,
+            x,
+            NODE_SIZE,
+            EDGE_SIZE,
+          );
+
+          const { width: left, height: top } = getTopLeft(
+            y,
+            x,
+            NODE_SIZE,
+            EDGE_SIZE,
+          );
+
+          return {
+            id: `${y}:${x}` as nid,
+            kind: even(y) && even(x) ? Kind.Node : Kind.Edge,
+            pos: {
+              x: PAD_X + left,
+              y: PAD_Y + top,
+            } as Point,
+            size: {
+              width: CELL_W,
+              height: CELL_H,
+            },
+            edges: {
+              top: undefined,
+              right: undefined,
+              down: undefined,
+              left: undefined,
+            },
+          };
+        }),
+    )
+    .flat();
+  return {
+    nodes: graph
+      .filter((n) => n.kind === Kind.Node)
+      .reduce(
+        (a, c) => {
+          a[c.id] = c;
+          return a;
+        },
+        {} as { [key: nid]: Node },
+      ),
+    edges: graph
+      .filter((n) => n.kind === Kind.Edge)
+      .reduce(
+        (a, c) => {
+          a[c.id] = c;
+          return a;
+        },
+        {} as { [key: nid]: Node },
+      ),
+  };
+}
+
+const first = (id: number) => id === 0;
+
+export function Maze({ id }: { id: number }) {
+  const svgRef = useRef<SVGSVGElement>(null);
+  const [maze, setMaze] = useState<Maze | null>(null);
+  useEffect(() => {
+    if (svgRef.current) {
+      const { width, height } = svgRef.current.getBoundingClientRect();
+      setMaze(generateMaze({ width, height }));
+    }
+  }, []);
+
+  const nodes = maze ? (
+    Object.values(maze.nodes).map(({ id, pos, size }, i) => {
+      return (
+        <rect
+          key={i}
+          x={pos.x}
+          y={pos.y}
+          width={size.width}
+          height={size.height}
+          fill="salmon"
+        />
+      );
+    })
+  ) : (
+    <></>
+  );
+
+  const edges = maze ? (
+    Object.values(maze.edges).map(({ id, pos, size }, i) => {
+      return (
+        <rect
+          key={i}
+          x={pos.x}
+          y={pos.y}
+          width={size.width}
+          height={size.height}
+          fill="lightsalmon"
+        />
+      );
+    })
+  ) : (
+    <></>
+  );
+
+  return (
+    <svg className="h-full w-full bg-teal-300" ref={svgRef}>
+      {edges}
+      {nodes}
+    </svg>
+  );
 }
--- src/routes/MazeRoute.tsx
+++ src/routes/MazeRoute.tsx
@@ -4,22 +4,22 @@ export function MazeRoute() {
   return (
     <div className="p-4 flex flex-col gap-y-4">
       <div className="h-32 bg-gray-500">
-        <Maze />
+        <Maze id={0} />
       </div>
       <div className="flex gap-x-4">
         <div className="h-64 w-32 bg-gray-500">
-          <Maze />
+          <Maze id={1} />
         </div>
         <div className="grow flex flex-col gap-y-4">
           <div className="h-20 grow bg-gray-500">
-            <Maze />
+            <Maze id={2} />
           </div>
           <div className="flex gap-x-4">
             <div className="h-32 grow-2 bg-gray-500">
-              <Maze />
+              <Maze id={3} />
             </div>
             <div className="h-32 grow bg-gray-500">
-              <Maze />
+              <Maze id={4} />
             </div>
           </div>
         </div>
